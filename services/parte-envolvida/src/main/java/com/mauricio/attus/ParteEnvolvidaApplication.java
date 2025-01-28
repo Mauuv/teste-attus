dc62c881003738a726b82f814f1bf905a598d531
Mauuv <mauricioverona1@gmail.com>
2025-01-27 22:19:02 -0300

    feat: adicionado o microsservico de parte envolvida e suas configurações

---
+++ services/parte-envolvida/src/main/java/com/mauricio/attus/ParteEnvolvidaApplication.java
@@ -0,0 +1,13 @@
+package com.mauricio.attus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParteEnvolvidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParteEnvolvidaApplication.class, args);
+	}
+
+}
